1bf7ecea15c97a6fbb6e0f66ee484ef1dce42b63
Chris <chris.gao77@gmail.com>
2022-10-27 18:22:36 -0400

    Update

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,6 +3,7 @@ import { ScrollUp } from "./ScrollUp"
 import { ScrollDown } from './ScrollDown'
 import { SiJavascript, SiTypescript, SiReact, SiNextdotjs, SiExpress, SiNodedotjs, SiJest, SiTestinglibrary, SiAmazonaws } from "react-icons/si"
 import "../styles/Skills.css"
+import { motion } from "framer-motion"
 
 export const Skills = () => {
 
@@ -35,73 +36,73 @@ export const Skills = () => {
           <h1 className="skills_title_period">.</h1>
         </div>
         <div className="skills_container">
-          {squareCounter === 0 ? <div className="skill_square_shake">
+          {squareCounter === 0 ? <motion.div whileTap = {{scale: 0.90}} className="skill_square_shake">
             {<SiJavascript style={{ width: "4vw", height: "4vw", borderRadius: "4%", color: "#18191F" }} />}
-          </div>
+          </motion.div>
             :
-            <div className="skill_square">
+            <motion.div whileTap = {{scale: 0.90}} className="skill_square">
               {<SiJavascript style={{ width: "4vw", height: "4vw", borderRadius: "4%", color: "#18191F" }} />}
-            </div>}
-          {squareCounter === 1 ? <div className="skill_square_shake">
+            </motion.div>}
+          {squareCounter === 1 ? <motion.div whileTap = {{scale: 0.90}} className="skill_square_shake">
             <SiReact style={{ width: "4vw", height: "4vw", color: "#18191F" }} />
-          </div>
+          </motion.div>
             :
-            <div className="skill_square">
+            <motion.div whileTap = {{scale: 0.90}} className="skill_square">
               <SiReact style={{ width: "4vw", height: "4vw", color: "#18191F" }} />
-            </div>}
-          {squareCounter === 2 ? <div className="skill_square_shake">
+            </motion.div>}
+          {squareCounter === 2 ? <motion.div whileTap = {{scale: 0.90}} className="skill_square_shake">
             <SiNextdotjs style={{ width: "4vw", height: "4vw", color: "#18191F" }} />
-          </div>
+          </motion.div>
             :
-            <div className="skill_square">
+            <motion.div whileTap = {{scale: 0.90}} className="skill_square">
               <SiNextdotjs style={{ width: "4vw", height: "4vw", color: "#18191F" }} />
-            </div>}
+            </motion.div>}
         </div>
         <div className="skills_container">
-          {squareCounter === 3 ? <div className="skill_square_shake">
+          {squareCounter === 3 ? <motion.div whileTap = {{scale: 0.90}} className="skill_square_shake">
             <SiNodedotjs style={{ width: "4vw", height: "4vw", color: "#18191F" }} />
-          </div>
+          </motion.div>
             :
-            <div className="skill_square">
+            <motion.div whileTap = {{scale: 0.90}} className="skill_square">
               <SiNodedotjs style={{ width: "4vw", height: "4vw", color: "#18191F" }} />
-            </div>}
-          {squareCounter === 4 ? <div className="skill_square_shake">
+            </motion.div>}
+          {squareCounter === 4 ? <motion.div whileTap = {{scale: 0.90}} className="skill_square_shake">
             <SiExpress style={{ width: "4vw", height: "4vw", color: "#18191F" }} />
-          </div>
+          </motion.div>
             :
-            <div className="skill_square">
+            <motion.div whileTap = {{scale: 0.90}} className="skill_square">
               <SiExpress style={{ width: "4vw", height: "4vw", color: "#18191F" }} />
-            </div>}
-          {squareCounter === 5 ? <div className="skill_square_shake">
+            </motion.div>}
+          {squareCounter === 5 ? <motion.div whileTap = {{scale: 0.90}} className="skill_square_shake">
             <SiTypescript style={{ width: "4vw", height: "4vw", color: "#18191F" }} />
-          </div>
+          </motion.div>
             :
-            <div className="skill_square">
+            <motion.div whileTap = {{scale: 0.90}} className="skill_square">
               <SiTypescript style={{ width: "4vw", height: "4vw", color: "#18191F" }} />
-            </div>}
+            </motion.div>}
         </div>
         <div className="skills_container">
-          {squareCounter === 6 ? <div className="skill_square_shake">
+          {squareCounter === 6 ? <motion.div whileTap = {{scale: 0.90}} className="skill_square_shake">
             <SiJest style={{ width: "4vw", height: "4vw", color: "#18191F" }} />
-          </div>
+          </motion.div>
             :
-            <div className="skill_square">
+            <motion.div whileTap = {{scale: 0.90}} className="skill_square">
               <SiJest style={{ width: "4vw", height: "4vw", color: "#18191F" }} />
-            </div>}
-          {squareCounter === 7 ? <div className="skill_square_shake">
+            </motion.div>}
+          {squareCounter === 7 ? <motion.div whileTap = {{scale: 0.90}} className="skill_square_shake">
             <SiTestinglibrary style={{ width: "4vw", height: "4vw", color: "#18191F" }} />
-          </div>
+          </motion.div>
             :
-            <div className="skill_square">
+            <motion.div whileTap = {{scale: 0.90}} className="skill_square">
               <SiTestinglibrary style={{ width: "4vw", height: "4vw", color: "#18191F" }} />
-            </div>}
-          {squareCounter === 8 ? <div className="skill_square_shake">
+            </motion.div>}
+          {squareCounter === 8 ? <motion.div whileTap = {{scale: 0.90}} className="skill_square_shake">
             <SiAmazonaws style={{ width: "4vw", height: "4vw", color: "#18191F" }} />
-          </div>
+          </motion.div>
             :
-            <div className="skill_square">
+            <motion.div whileTap = {{scale: 0.90}} className="skill_square">
               <SiAmazonaws style={{ width: "4vw", height: "4vw", color: "#18191F" }} />
-            </div>}
+            </motion.div>}
         </div>
       </div>
       <ScrollDown nextPage="Projects" />
