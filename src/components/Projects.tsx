0cd8ad68e5cb45739325d43bc0bb0ea774e95e31
Chris <chris.gao77@gmail.com>
2022-10-29 23:10:01 -0400

    Update

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { ScrollDown } from './ScrollDown'
+import { ScrollUp } from './ScrollUp'
 
 export const Projects = () => {
   return (
-    <div style={{ height: "110vh", flexShrink: "0", scrollSnapAlign: "start" }} id="Projects">
-        
+    <div style={{ height: "107.5vh", flexShrink: "0", scrollSnapAlign: "start", paddingTop: "2.5vh" }} id="Projects">
+        <ScrollUp lastPage="Skills" />
+        <div className="projects_main_container">
+
+        </div>
     </div>
   )
 }
