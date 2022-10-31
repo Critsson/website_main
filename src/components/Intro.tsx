36019e349b3d2c7a9edcfd9763a3c1b734535ad3
Chris <chris.gao77@gmail.com>
2022-10-31 19:29:39 -0400

    Revert "Update to spacing"
    This reverts commit 0df93380831fff94c074e9143c36d5bd7c35e198.

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -7,8 +7,8 @@ import { ScrollDown } from './ScrollDown'
 
 export const Intro = () => {
   return (
-    <Stack id="Home" direction="column" alignItems="center" justifyContent="center" height="100%" flexShrink={0} sx={{scrollSnapAlign: "start"}}>
-        <div className="picture_wrapper" style={{marginTop: "4vw"}}>
+    <Stack id="Home" direction="column" alignItems="center" justifyContent="center" height="110vh" flexShrink={0} sx={{scrollSnapAlign: "start"}}>
+        <div className="picture_wrapper">
         </div>
         <IntroTextWrapper />
         <Socials />
