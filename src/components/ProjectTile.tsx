c9b9922123d9f6c0d04cfef6f3c9457608503ecb
Chris <chris.gao77@gmail.com>
2022-11-02 17:36:07 -0400

    Update to projects

--- src/components/ProjectTile.tsx
+++ src/components/ProjectTile.tsx
@@ -12,9 +12,9 @@ interface ProjectTileProps {
 export const ProjectTile = () => {
   return (
     <div className="project_main_container">
-        <h1 style={{color: "white"}}>This is test text</h1>
-        <Accordion sx={{backgroundColor: "transparent", borderRadius: "0%", fontFamily: "Roboto", color: "white", width: "100%"}}>
-            <AccordionSummary expandIcon={<ArrowDropDownIcon sx={{color: "white"}} />}>Read more</AccordionSummary>
+        <h1 style={{color: "#18191F", fontSize: "1vw", fontFamily: "Roboto", paddingLeft: ".7vw"}}>This is test text</h1>
+        <Accordion square={true} sx={{backgroundColor: "transparent", borderRadius: "0%", fontFamily: "Roboto", color: "#18191F", width: "100%", fontWeight: "700", fontSize: ".8vw"}}>
+            <AccordionSummary expandIcon={<ArrowDropDownIcon sx={{color: "#18191F", fontSize: "1.3vw"}} />}>Read more</AccordionSummary>
             <AccordionDetails>This is more of the test</AccordionDetails>
         </Accordion>
     </div>
