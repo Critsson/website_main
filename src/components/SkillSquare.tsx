93faf316a7af7043e1b34a3c5f4c9ae2e7bb9927
Chris <chris.gao77@gmail.com>
2022-11-03 20:00:47 -0400

    Skills mobile page update

--- src/components/SkillSquare.tsx
+++ src/components/SkillSquare.tsx
@@ -7,6 +7,7 @@ import { MdOutlineMonitor } from "react-icons/md"
 import { Tooltip } from "@mui/material"
 import "../styles/SkillSquare.css"
 import Zoom from "@mui/material/Zoom"
+import getWindowWidth from '../getWindowWidth'
 
 interface SkillsSquareProps {
     id: number,
@@ -22,9 +23,20 @@ interface SkillsSquareProps {
 
 export const SkillSquare = ({ icon, flipped, id, flip, backIcons, shakeDelay }: SkillsSquareProps) => {
 
+    const [windowWidth, setWindowWidth] = React.useState(getWindowWidth())
+
+    React.useEffect(() => {
+        function windowResized() {
+            setWindowWidth(getWindowWidth());
+        }
+
+        window.addEventListener('resize', windowResized);
+        return () => window.removeEventListener('resize', windowResized);
+    }, [])
+
     const variants = {
         unflipped: { translateX: [6, -12, 12, -12, 6], transition: { delay: shakeDelay, repeat: Infinity, repeatDelay: 74, duration: .4 } },
-        flipped: {transition: {delay: 0}}
+        flipped: { transition: { delay: 0 } }
     }
 
     let iconElement;
@@ -52,37 +64,61 @@ export const SkillSquare = ({ icon, flipped, id, flip, backIcons, shakeDelay }: 
         }
     })
 
-    if (icon === "Javascript") {
-        iconElement = <SiJavascript style={{ width: "4vw", height: "4vw", borderRadius: "4%", color: "#18191F" }} />
-    } else if (icon === "Typescript") {
-        iconElement = <SiTypescript style={{ width: "4vw", height: "4vw", borderRadius: "4%", color: "#18191F" }} />
-    } else if (icon === "React") {
-        iconElement = <SiReact style={{ width: "4vw", height: "4vw", borderRadius: "4%", color: "#18191F" }} />
-    } else if (icon === "Nextdotjs") {
-        iconElement = <SiNextdotjs style={{ width: "4vw", height: "4vw", borderRadius: "4%", color: "#18191F" }} />
-    } else if (icon === "Express") {
-        iconElement = <SiExpress style={{ width: "4vw", height: "4vw", borderRadius: "4%", color: "#18191F" }} />
-    } else if (icon === "Nodedotjs") {
-        iconElement = <SiNodedotjs style={{ width: "4vw", height: "4vw", borderRadius: "4%", color: "#18191F" }} />
-    } else if (icon === "Jest") {
-        iconElement = <SiJest style={{ width: "4vw", height: "4vw", borderRadius: "4%", color: "#18191F" }} />
-    } else if (icon === "Postgresql") {
-        iconElement = <SiPostgresql style={{ width: "4vw", height: "4vw", borderRadius: "4%", color: "#18191F" }} />
-    } else if (icon === "Amazonaws") {
-        iconElement = <SiAmazonaws style={{ width: "4vw", height: "4vw", borderRadius: "4%", color: "#18191F" }} />
+    if (windowWidth > 641) {
+        if (icon === "Javascript") {
+            iconElement = <SiJavascript style={{ width: "4vw", height: "4vw", borderRadius: "4%", color: "#18191F" }} />
+        } else if (icon === "Typescript") {
+            iconElement = <SiTypescript style={{ width: "4vw", height: "4vw", borderRadius: "4%", color: "#18191F" }} />
+        } else if (icon === "React") {
+            iconElement = <SiReact style={{ width: "4vw", height: "4vw", borderRadius: "4%", color: "#18191F" }} />
+        } else if (icon === "Nextdotjs") {
+            iconElement = <SiNextdotjs style={{ width: "4vw", height: "4vw", borderRadius: "4%", color: "#18191F" }} />
+        } else if (icon === "Express") {
+            iconElement = <SiExpress style={{ width: "4vw", height: "4vw", borderRadius: "4%", color: "#18191F" }} />
+        } else if (icon === "Nodedotjs") {
+            iconElement = <SiNodedotjs style={{ width: "4vw", height: "4vw", borderRadius: "4%", color: "#18191F" }} />
+        } else if (icon === "Jest") {
+            iconElement = <SiJest style={{ width: "4vw", height: "4vw", borderRadius: "4%", color: "#18191F" }} />
+        } else if (icon === "Postgresql") {
+            iconElement = <SiPostgresql style={{ width: "4vw", height: "4vw", borderRadius: "4%", color: "#18191F" }} />
+        } else if (icon === "Amazonaws") {
+            iconElement = <SiAmazonaws style={{ width: "4vw", height: "4vw", borderRadius: "4%", color: "#18191F" }} />
+        } else {
+            iconElement = <></>
+        }
     } else {
-        iconElement = <></>
+        if (icon === "Javascript") {
+            iconElement = <SiJavascript style={{ width: "10vw", height: "10vw", borderRadius: "4%", color: "#18191F" }} />
+        } else if (icon === "Typescript") {
+            iconElement = <SiTypescript style={{ width: "10vw", height: "10vw", borderRadius: "4%", color: "#18191F" }} />
+        } else if (icon === "React") {
+            iconElement = <SiReact style={{ width: "10vw", height: "10vw", borderRadius: "4%", color: "#18191F" }} />
+        } else if (icon === "Nextdotjs") {
+            iconElement = <SiNextdotjs style={{ width: "10vw", height: "10vw", borderRadius: "4%", color: "#18191F" }} />
+        } else if (icon === "Express") {
+            iconElement = <SiExpress style={{ width: "10vw", height: "10vw", borderRadius: "4%", color: "#18191F" }} />
+        } else if (icon === "Nodedotjs") {
+            iconElement = <SiNodedotjs style={{ width: "10vw", height: "10vw", borderRadius: "4%", color: "#18191F" }} />
+        } else if (icon === "Jest") {
+            iconElement = <SiJest style={{ width: "10vw", height: "10vw", borderRadius: "4%", color: "#18191F" }} />
+        } else if (icon === "Postgresql") {
+            iconElement = <SiPostgresql style={{ width: "10vw", height: "10vw", borderRadius: "4%", color: "#18191F" }} />
+        } else if (icon === "Amazonaws") {
+            iconElement = <SiAmazonaws style={{ width: "10vw", height: "10vw", borderRadius: "4%", color: "#18191F" }} />
+        } else {
+            iconElement = <></>
+        }
     }
 
 
     return (
         <>
-            {!flipped ? <AnimatePresence exitBeforeEnter={true}><motion.div initial={{opacity: 0}} whileInView={{opacity:1, transition: {delay: .15}}} animate={!flipped ? "unflipped" : "flipped"} variants={variants} onClick={() => flip(id)} whileTap={{ scale: .8 }} className="skill_square">
+            {!flipped ? <AnimatePresence exitBeforeEnter={true}><motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1, transition: { delay: .15 } }} animate={!flipped ? "unflipped" : "flipped"} variants={variants} onClick={() => flip(id)} whileTap={{ scale: .8 }} className="skill_square">
                 {iconElement}
             </motion.div>
             </AnimatePresence>
                 :
-                <AnimatePresence exitBeforeEnter={true}><motion.div initial={{opacity: 0}} whileInView={{opacity:1, transition: {delay: .15}}} onClick={() => flip(id)} animate={{ rotateY: 180 }} whileTap={{ scale: .8 }} className="skill_square">
+                <AnimatePresence exitBeforeEnter={true}><motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1, transition: { delay: .15 } }} onClick={() => flip(id)} animate={{ rotateY: 180 }} whileTap={{ scale: .8 }} className="skill_square">
                     <div className="back_icons_container">
                         {backElements}
                     </div>
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,10 +3,21 @@ import { ScrollUp } from "./ScrollUp"
 import { ScrollDown } from './ScrollDown'
 import "../styles/Skills.css"
 import { SkillSquare } from './SkillSquare'
-import {motion} from "framer-motion"
+import { motion } from "framer-motion"
+import getWindowWidth from '../getWindowWidth'
 
 export const Skills = () => {
   const [flippedArray, setFlippedArray] = React.useState<boolean[]>([false, false, false, false, false, false, false, false, false])
+  const [windowWidth, setWindowWidth] = React.useState(getWindowWidth())
+
+  React.useEffect(() => {
+    function windowResized() {
+      setWindowWidth(getWindowWidth());
+    }
+
+    window.addEventListener('resize', windowResized);
+    return () => window.removeEventListener('resize', windowResized);
+  }, [])
 
   const flip = (id: number) => {
     setFlippedArray((): boolean[] => {
@@ -22,30 +33,58 @@ export const Skills = () => {
   }
 
   return (
-    <div id="Skills" style={{ height: "100vh", flexShrink: "0", scrollSnapAlign: "start", display: "flex", flexDirection: "column", alignItems: "center", paddingTop: "2vh" }}>
-      <ScrollUp lastPage="Home" />
-      <div className="skills_main_container">
-        <motion.div initial = {{opacity: 0}} whileInView={{opacity: 1, transition: {delay: .15}}} className="skills_title_container">
-          <h1>What I Work With</h1>
-          <h1 className="skills_title_period">.</h1>
-        </motion.div>
-        <div className="skills_container">
-          <SkillSquare shakeDelay={50} flip={flip} id={0} icon="Javascript" backIcons={["Back End", "Front End"]} flipped={flippedArray[0]} />
-          <SkillSquare shakeDelay={2} flip={flip} id={1} icon="React" backIcons={["Front End"]} flipped={flippedArray[1]} />
-          <SkillSquare shakeDelay={10} flip={flip} id={2} icon="Nextdotjs" backIcons={["Back End", "Front End"]} flipped={flippedArray[2]} />
+    <>
+      {windowWidth > 640 ? <div id="Skills" style={{ height: "100vh", flexShrink: "0", scrollSnapAlign: "start", display: "flex", flexDirection: "column", alignItems: "center", paddingTop: "2vh" }}>
+        <ScrollUp lastPage="Home" />
+        <div className="skills_main_container">
+          <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1, transition: { delay: .15 } }} className="skills_title_container">
+            <h1>What I Work With</h1>
+            <h1 className="skills_title_period">.</h1>
+          </motion.div>
+          <div className="skills_container">
+            <SkillSquare shakeDelay={50} flip={flip} id={0} icon="Javascript" backIcons={["Back End", "Front End"]} flipped={flippedArray[0]} />
+            <SkillSquare shakeDelay={2} flip={flip} id={1} icon="React" backIcons={["Front End"]} flipped={flippedArray[1]} />
+            <SkillSquare shakeDelay={10} flip={flip} id={2} icon="Nextdotjs" backIcons={["Back End", "Front End"]} flipped={flippedArray[2]} />
+          </div>
+          <div className="skills_container">
+            <SkillSquare shakeDelay={66} flip={flip} id={3} icon="Nodedotjs" backIcons={["Back End"]} flipped={flippedArray[3]} />
+            <SkillSquare shakeDelay={58} flip={flip} id={4} icon="Express" backIcons={["Back End"]} flipped={flippedArray[4]} />
+            <SkillSquare shakeDelay={34} flip={flip} id={5} icon="Typescript" backIcons={["Back End", "Front End"]} flipped={flippedArray[5]} />
+          </div>
+          <div className="skills_container">
+            <SkillSquare shakeDelay={18} flip={flip} id={6} icon="Postgresql" backIcons={["Back End", "Database"]} flipped={flippedArray[6]} />
+            <SkillSquare shakeDelay={42} flip={flip} id={7} icon="Jest" backIcons={["Testing"]} flipped={flippedArray[7]} />
+            <SkillSquare shakeDelay={26} flip={flip} id={8} icon="Amazonaws" backIcons={["Cloud", "Database"]} flipped={flippedArray[8]} />
+          </div>
         </div>
-        <div className="skills_container">
-          <SkillSquare shakeDelay={66} flip={flip} id={3} icon="Nodedotjs" backIcons={["Back End"]} flipped={flippedArray[3]} />
-          <SkillSquare shakeDelay={58} flip={flip} id={4} icon="Express" backIcons={["Back End"]} flipped={flippedArray[4]} />
-          <SkillSquare shakeDelay={34} flip={flip} id={5} icon="Typescript" backIcons={["Back End", "Front End"]} flipped={flippedArray[5]} />
-        </div>
-        <div className="skills_container">
-          <SkillSquare shakeDelay={18} flip={flip} id={6} icon="Postgresql" backIcons={["Back End", "Database"]} flipped={flippedArray[6]} />
-          <SkillSquare shakeDelay={42} flip={flip} id={7} icon="Jest" backIcons={["Testing"]} flipped={flippedArray[7]} />
-          <SkillSquare shakeDelay={26} flip={flip} id={8} icon="Amazonaws" backIcons={["Cloud", "Database"]} flipped={flippedArray[8]} />
-        </div>
+        <ScrollDown nextPage="Projects" />
       </div>
-      <ScrollDown nextPage="Projects" />
-    </div>
+        :
+        <div id="Skills" style={{ height: "100vh", flexShrink: "0", scrollSnapAlign: "start", display: "flex", flexDirection: "column", alignItems: "center", paddingTop: "7vh" }}>
+          <ScrollUp lastPage="Home" />
+          <div className="skills_main_container">
+            <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1, transition: { delay: .15 } }} className="skills_title_container">
+              <h1>What I Work With</h1>
+              <h1 className="skills_title_period">.</h1>
+            </motion.div>
+            <div className="skills_container">
+              <SkillSquare shakeDelay={50} flip={flip} id={0} icon="Javascript" backIcons={["Back End", "Front End"]} flipped={flippedArray[0]} />
+              <SkillSquare shakeDelay={2} flip={flip} id={1} icon="React" backIcons={["Front End"]} flipped={flippedArray[1]} />
+              <SkillSquare shakeDelay={10} flip={flip} id={2} icon="Nextdotjs" backIcons={["Back End", "Front End"]} flipped={flippedArray[2]} />
+            </div>
+            <div className="skills_container">
+              <SkillSquare shakeDelay={66} flip={flip} id={3} icon="Nodedotjs" backIcons={["Back End"]} flipped={flippedArray[3]} />
+              <SkillSquare shakeDelay={58} flip={flip} id={4} icon="Express" backIcons={["Back End"]} flipped={flippedArray[4]} />
+              <SkillSquare shakeDelay={34} flip={flip} id={5} icon="Typescript" backIcons={["Back End", "Front End"]} flipped={flippedArray[5]} />
+            </div>
+            <div className="skills_container">
+              <SkillSquare shakeDelay={18} flip={flip} id={6} icon="Postgresql" backIcons={["Back End", "Database"]} flipped={flippedArray[6]} />
+              <SkillSquare shakeDelay={42} flip={flip} id={7} icon="Jest" backIcons={["Testing"]} flipped={flippedArray[7]} />
+              <SkillSquare shakeDelay={26} flip={flip} id={8} icon="Amazonaws" backIcons={["Cloud", "Database"]} flipped={flippedArray[8]} />
+            </div>
+          </div>
+          <ScrollDown nextPage="Projects" />
+        </div>}
+    </>
   )
 }
