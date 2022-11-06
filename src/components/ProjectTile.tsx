045be1a965fc5e6fcba13692f7bfdb286efb87f3
Chris <chris.gao77@gmail.com>
2022-11-06 17:41:17 -0500

    Project tile button update

--- src/components/ProjectTile.tsx
+++ src/components/ProjectTile.tsx
@@ -1,23 +1,35 @@
 import React from 'react'
 import "../styles/ProjectTile.css"
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import { SiJavascript, SiTypescript, SiReact, SiNextdotjs, SiExpress, SiNodedotjs, SiJest, SiPostgresql, SiAmazonaws, SiIcloud, SiHtml5, SiCss3, SiNetlify, SiMaterialui } from "react-icons/si"
 import { motion, AnimatePresence } from "framer-motion"
+import { Button } from "@mui/material"
+import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
 
 interface ProjectTileProps {
-  id?: number
-  icons?: string[],
-  thumbnail?: string,
-  link?: string,
+  id: number
+  icons: string[],
+  thumbnail: string,
+  link: string,
+  hovered: boolean,
+  handleOnHover: (id: number) => void,
+  handleOffHover: (id: number) => void
 }
 
-export const ProjectTile = ({ id, icons, thumbnail, link }: ProjectTileProps) => {
+export const ProjectTile = ({ id, icons, thumbnail, link, hovered, handleOnHover, handleOffHover }: ProjectTileProps) => {
   return (
-    <a href={link} target="_blank" >
-      <AnimatePresence>
-        <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1, transition: { delay: .15 } }} className="project_main_container" style={{ backgroundImage: `url(/${thumbnail})` }}>
+    <>
+      {hovered === false ?
+        <motion.div onMouseEnter={() => handleOnHover(id)} initial={{ opacity: 0 }} whileInView={{ opacity: 1, transition: { delay: .15 } }} className="project_main_container" style={{ backgroundImage: `url(/${thumbnail})` }}>
         </motion.div>
-      </AnimatePresence>
-    </a>
+        :
+        <motion.div onMouseLeave={() => handleOffHover(id)} initial={{ opacity: 0 }} whileInView={{ opacity: 1, transition: { delay: .15 } }} className="project_main_container" style={{ backgroundImage: `url(/${thumbnail})` }}>
+          <AnimatePresence>
+            <motion.div className="project_tile_hover_container" key={id} initial={{ height: 0 }} animate={{ height: 200 }} exit={{ height: 0 }} transition={{ duration: .5 }} style={{ height: "100%", width: "100%", backgroundColor: "rgba(0, 0, 0, 0.6)" }}>
+              <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: .1}}><Button sx={{ display: "flex", alignItems: "center", fontSize: ".8vw", width: "5vw", height: "2vw" }} color="primary" variant="contained" endIcon={<MeetingRoomIcon sx={{ width: "1.1vw", height: "1.1vw", marginBottom: ".1vw" }} />}>Visit</Button></motion.div>
+            </motion.div>
+          </AnimatePresence>
+        </motion.div>
+      }
+    </>
   )
 }
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,12 +2,30 @@ import React from 'react'
 import { ScrollDown } from './ScrollDown'
 import { ScrollUp } from './ScrollUp'
 import { motion } from "framer-motion"
-import { Grid } from '@mui/material'
 import { ProjectTile } from './ProjectTile'
 import "../styles/Projects.css"
 
 
 export const Projects = () => {
+
+  const [hoveredArray, setHoveredArray] = React.useState([{id: 0, hovered: false}, {id: 1, hovered: false}, {id: 2, hovered: false},{id: 3, hovered: false},{id: 4, hovered: false},{id: 5, hovered: false},{id: 6, hovered: false}])
+
+  const handleOnHover = (id: number) => {
+      setHoveredArray((prevState) => {
+          const holder = [...prevState]
+          holder[id] = {id: id, hovered: true}
+          return holder
+      })
+  }
+
+  const handleOffHover = (id: number) => {
+    setHoveredArray((prevState) => {
+        const holder = [...prevState]
+        holder[id] = {id: id, hovered: false}
+        return holder
+    })
+}
+
   return (
     <div style={{ height: "100vh", flexShrink: "0", scrollSnapAlign: "start", display: "flex", flexDirection: "column", alignItems: "center", paddingTop: "2vh" }} id="Projects">
       <ScrollUp lastPage="Skills" />
@@ -20,62 +38,83 @@ export const Projects = () => {
           <div className="projects_row_container">
             <div style={{ height: "20vh", display: "flex", flexDirection: "column", alignItems: "center" }}>
               <ProjectTile
-                id={0}
+                handleOnHover={handleOnHover}
+                handleOffHover={handleOffHover}
+                id={hoveredArray[0].id}
                 icons={["SiReact", "SiNextdotjs", "SiHtml5", "SiCss3", "SiJavascript"]}
                 thumbnail="thumbnails/climbgg_thumbnail.JPG"
                 link="https://climbgg.netlify.app/"
+                hovered={hoveredArray[0].hovered}              
               />
             </div>
             <div style={{ height: "20vh", display: "flex", flexDirection: "column", alignItems: "center" }}>
               <ProjectTile
-                id={1}
+                handleOnHover={handleOnHover}
+                handleOffHover={handleOffHover}
+                id={hoveredArray[1].id}
                 icons={["SiReact", "SiNextdotjs", "SiHtml5", "SiCss3", "SiJavascript", "SiNetlify"]}
                 thumbnail="thumbnails/glorious_diaries_thumbnail.JPG"
                 link="https://gloriousdiaries.netlify.app/"
+                hovered={hoveredArray[1].hovered}              
               />
             </div>
             <div style={{ height: "20vh", display: "flex", flexDirection: "column", alignItems: "center" }}>
               <ProjectTile
-                id={2}
+                handleOnHover={handleOnHover}
+                handleOffHover={handleOffHover}
+                id={hoveredArray[2].id}
                 icons={["SiReact", "SiHtml5", "SiCss3", "SiTypescript", "SiMaterialui"]}
                 thumbnail="thumbnails/personal_website_thumbnail.JPG"
                 link="https://chrisgao.netlify.app/"
+                hovered={hoveredArray[2].hovered}              
               />
             </div>
           </div>
           <div className="projects_row_container">
             <div style={{ height: "20vh", display: "flex", flexDirection: "column", alignItems: "center" }}>
               <ProjectTile
-                id={3}
+                handleOnHover={handleOnHover}
+                handleOffHover={handleOffHover}
+                id={hoveredArray[3].id}
                 icons={["SiReact", "SiHtml5", "SiCss3", "SiTJavascript, SiMaterialui"]}
                 thumbnail="thumbnails/dictionary_thumbnail.JPG"
                 link="https://chrisdictionary.netlify.app/"
+                hovered={hoveredArray[3].hovered}              
               />
             </div>
             <div style={{ height: "20vh", display: "flex", flexDirection: "column", alignItems: "center" }}>
               <ProjectTile
-                id={4}
+                handleOnHover={handleOnHover}
+                handleOffHover={handleOffHover}
+                id={hoveredArray[4].id}
                 icons={["SiReact", "SiNextdotjs", "SiHtml5", "SiCss3", "SiJavascript"]}
                 thumbnail="thumbnails/climbgg_thumbnail.JPG"
                 link="https://climbgg.netlify.app/"
+                hovered={hoveredArray[4].hovered}              
               />
             </div>
             <div style={{ height: "20vh", display: "flex", flexDirection: "column", alignItems: "center" }}>
               <ProjectTile
-                id={5}
+                handleOnHover={handleOnHover}
+                handleOffHover={handleOffHover}
+                id={hoveredArray[5].id}
                 icons={["SiReact", "SiNextdotjs", "SiHtml5", "SiCss3", "SiJavascript"]}
                 thumbnail="thumbnails/climbgg_thumbnail.JPG"
                 link="https://climbgg.netlify.app/"
+                hovered={hoveredArray[5].hovered}              
               />
             </div>
           </div>
           <div className="projects_row_container">
             <div style={{ height: "20vh", display: "flex", flexDirection: "column", alignItems: "center" }}>
               <ProjectTile
-                id={6}
+                handleOnHover={handleOnHover}
+                handleOffHover={handleOffHover}
+                id={hoveredArray[6].id}
                 icons={["SiReact", "SiNextdotjs", "SiHtml5", "SiCss3", "SiJavascript"]}
                 thumbnail="thumbnails/climbgg_thumbnail.JPG"
                 link="https://climbgg.netlify.app/"
+                hovered={hoveredArray[6].hovered}              
               />
             </div>
           </div>
